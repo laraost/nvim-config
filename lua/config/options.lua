9e2c081430d31c092015bef92badfdd3aa76131e
Lara Ost <git@laraost.eu>
2025-04-02 14:45:20 +0200

    Add an autocmd to toggle hybrid numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,26 @@ vim.cmd.set("expandtab")
 vim.cmd.set("termguicolors")
 vim.cmd.colorscheme("catppuccin")
 vim.cmd.set("background=light")
+
+-- Line numbering
+vim.cmd.set("number relativenumber")
+local numbergroup = vim.api.nvim_create_augroup("numbertoggle", { clear=true })
+vim.api.nvim_create_autocmd(
+    { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+    {
+        pattern = "*",
+        group = numbergroup,
+        command = "if &nu && mode() != 'i' | set rnu | endif",
+        desc = "turn relative numbers on"
+    }
+)
+vim.api.nvim_create_autocmd(
+    { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+    {
+        pattern = "*",
+        group = numbergroup,
+        command = "if &nu | set nornu | endif",
+        desc = "turn relative numbers off"
+    }
+)
+
