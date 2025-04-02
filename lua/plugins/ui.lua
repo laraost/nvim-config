e8eb84b3c0ccd3da7ad6509811098024c83bad2d
Lara Ost <git@laraost.eu>
2025-04-02 15:52:51 +0200

    Configure buffer line: add buffer number and make focus bold

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -44,6 +44,38 @@ return {
                           and get_hex('Normal', 'fg')
                            or get_hex('ColorColumn', 'bg')
                     end,
+                    bold = function(buffer) return buffer.is_focused end,
+                },
+                components = {
+                    {
+                        text = function(buffer) return " " .. buffer.devicon.icon end,
+                        fg = function(buffer) return buffer.devicon.color end,
+                    },
+                    {
+                        text = function(buffer) return buffer.number .. ": " end,
+                    },
+                    {
+                        text = function(buffer) return buffer.unique_prefix end,
+                        fg = function() return get_hex("Comment", "fg") end,
+                        italic = true,
+                    },
+                    { text = function(buffer) return buffer.filename .. " " end },
+                    {
+                        ---@param buffer Buffer
+                        text = function(buffer)
+                            if buffer.is_modified then
+                                return ""
+                            end
+                            if buffer.is_hovered then
+                                return "󰅙"
+                            end
+                            return "󰅖"
+                        end,
+                        on_click = function(_, _, _, _, buffer)
+                            buffer:delete()
+                        end,
+                    },
+                    { text = " ", },
                 },
             })
         end,
