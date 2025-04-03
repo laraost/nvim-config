f82e37ca706920266c0c803ed2cf6695d2eee764
Lara Ost <git@laraost.eu>
2025-04-03 22:55:58 +0200

    Configure keys after plugins are loaded

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Setting important keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-require("config.keys")
 require("config.lazy")
+require("config.keys")
 require("config.options")
 require("config.lsp")
