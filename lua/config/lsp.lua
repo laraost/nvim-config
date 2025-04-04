1dcbb38dc498c9ddab74c02ba73e1c2936f62a29
Lara Ost <git@laraost.eu>
2025-04-04 19:01:05 +0200

    Add keymap for diagnostics

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -14,6 +14,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = {buffer = event.buf}
 
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', '<Leader>gh', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
     vim.keymap.set('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
     vim.keymap.set('n', '<Leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
     vim.keymap.set('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
